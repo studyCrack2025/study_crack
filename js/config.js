462d5045c126ea0cc671d04869b26d4aecd1ceba
TaeryungIm <studycracked@gmail.com>
2026-01-08 03:05:38 +0900

    config clint change update

--- js/config.js
+++ js/config.js
@@ -2,6 +2,6 @@
 const CONFIG = {
     cognito: {
         userPoolId: 'ap-northeast-2_00mP8t8UM', // AWS Cognito에서 확인
-        clientId: '13v5nv5vmooq3ettmq10i9m30m'  // AWS Cognito에서 확인
+        clientId: '2lovlq38kvgn2dckppn91iqq2l'  // AWS Cognito에서 확인
     }
 };
