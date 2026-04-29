2af050a8695ce398503bcf373538d0dbf9939718
studyCrack2025 <studycracked@gmail.com>
2026-04-29 12:46:17 +0900

    Set boot flag immediately to avoid fallback race

--- js/studycrack-mobile.js
+++ js/studycrack-mobile.js
@@ -1,5 +1,6 @@
 const { useState, useEffect, useRef } = React;
 if (typeof window !== 'undefined') window.__studycrackAppBooted = true;
+if (typeof window !== 'undefined') window.__studycrackAppBooted = true;
 
 const CRACKY_SRC = './assets/images/3A1D897F-252E-4096-AEF2-C4FA7CA6689D.png';
 const ONBOARDING_LOGO_SRC = './assets/images/og-image.jpg';
