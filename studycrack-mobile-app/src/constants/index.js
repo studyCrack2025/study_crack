a3f1442d326e16fbb0b246811ee92fa3175b326c
TaeryungIm <studycracked@gmail.com>
2026-06-12 00:42:51 +0900

    Split mobile app constants

---
+++ studycrack-mobile-app/src/constants/index.js
@@ -0,0 +1,7 @@
+export * from './assets.js';
+export * from './mock-data.js';
+export * from './options.js';
+export * from './plans.js';
+export * from './ranking.js';
+export * from './terms.js';
+export * from './universities.js';
