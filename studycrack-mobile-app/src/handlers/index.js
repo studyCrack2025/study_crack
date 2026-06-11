f114bf74eb24c4d7c82d168014ac4fbbafee79fa
TaeryungIm <studycracked@gmail.com>
2026-06-12 03:35:25 +0900

    Add mobile service handlers

--- studycrack-mobile-app/src/handlers/index.js
+++ studycrack-mobile-app/src/handlers/index.js
@@ -4,3 +4,4 @@ export * from './dispatch.js';
 export * from './navigation-handlers.js';
 export * from './planner-handlers.js';
 export * from './profile-handlers.js';
+export * from './service-handlers.js';
