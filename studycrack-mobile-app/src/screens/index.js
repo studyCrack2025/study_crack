02e819676744277cece2825d5f7e5bf8547c1d3d
TaeryungIm <studycracked@gmail.com>
2026-06-12 02:00:36 +0900

    Split mobile profile renderers

--- studycrack-mobile-app/src/screens/index.js
+++ studycrack-mobile-app/src/screens/index.js
@@ -4,4 +4,5 @@ export * from './home/index.js';
 export * from './mypage/index.js';
 export * from './onboarding/index.js';
 export * from './planner/index.js';
+export * from './profile/index.js';
 export * from './service/index.js';
